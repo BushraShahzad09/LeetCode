db7b1a244d5703d7a9bfe5e29b11bd5b45ccd6bf
BushraShahzad09 <100803571+BushraShahzad09@users.noreply.github.com>
2023-06-10 23:58:32 +0530

    Added solution - LeetHub

---
+++ Arranging the array - GFG/arranging-the-array.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            vector<int>ps;
+            vector<int>ng;
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]<0)
+                ng.push_back(arr[i]);
+            }
+            for(int i=0;i<n;i++)
+            {
+                if(arr[i]>=0)
+                ps.push_back(arr[i]);
+            }
+           // arr.clear();
+            for(int i=0;i<ng.size();i++)
+            {
+                arr[i]=ng[i];
+            }
+             for(int i=0;i<ps.size();i++)
+            {
+                arr[i+ng.size()]=ps[i];
+            }
+            
+            
+        }
+};
+
+//{ Driver Code Starts.
+void Rearrange(int arr[], int n);
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        cin>>arr[i];
+        long long j=0;
+        Solution ob;
+        ob.Rearrange(arr, n);
+      
+        for (int i = 0; i < n; i++) 
+            cout << arr[i] << " "; 
+        cout << endl;  
+    }
+    return 0; 
+} 
+// } Driver Code Ends
